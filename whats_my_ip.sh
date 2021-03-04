13e6e57dcc332a631acf104a16cacc13fc2f4dd6
Zeeshanalik <zeeshan.ali@datumbrain.com>
2021-03-04 19:28:14 +0500

    Task3 Completed

--- whats_my_ip.sh
+++ whats_my_ip.sh
@@ -1,2 +1,8 @@
 #! /bin/bash
-ip -f inet addr show br-01e77cef63dc | awk '/inet /{ print $2}'|cut -d'/' -f1
+compare=`/sbin/ifconfig | grep 192` # to compare and find all the strings in ifconfig that starts with 192 and storing the result
+IPcomp=`expr index "$compare" "192"` 	# finding the numerical position of 192 from the stored string
+compare=${compare:IPcomp - 1}  	# extracting the substring from compare variable by omitting	
+space=`expr index "$compare" " "`	#finding position of empty spaces
+compare=${compare:0:space}		#ommiting space upto first space
+echo $compare				#result
+
